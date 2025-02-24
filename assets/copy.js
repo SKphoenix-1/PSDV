c3979fe8dbf9c8320c2fa1b2077e51688bfe58cd
SKphoenix-1 <satvikkadian1@gmail.com>
2025-02-25 04:04:56 +0530

    Update copy.js

--- assets/copy.js
+++ assets/copy.js
@@ -1,18 +1,5 @@
-document.querySelectorAll('.copy-btn').forEach(button => {
-    button.addEventListener('click', function() {
-        const codeBlock = this.nextElementSibling.querySelector('code').innerText;
-        navigator.clipboard.writeText(codeBlock).then(() => {
-            this.innerHTML = "Copied!"; // Change to "Copied!"
-            this.classList.add('copied');
 
-            setTimeout(() => {
-                this.innerHTML = '<i class="fas fa-clipboard"></i>'; // Restore icon
-                this.classList.remove('copied');
-            }, 2000);
-        });
-    });
-});
-/*document.querySelectorAll(".copy-btn").forEach(button => {
+document.querySelectorAll(".copy-btn").forEach(button => {
     button.addEventListener("click", function () {
         let codeBlock = this.closest(".code-container").querySelector("pre code"); // Select the correct code block
         let text = codeBlock.innerText; // Get the code text
@@ -23,4 +10,4 @@ document.querySelectorAll('.copy-btn').forEach(button => {
             }, 1500);
         }).catch(err => console.error("Failed to copy:", err));
     });
-});*/
+});
